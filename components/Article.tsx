8ca7fca310de228a196551642f0c3781d5938f47
mass2527 <nzp01@naver.com>
2022-02-27 14:54:12 +0900

    add Article component

---
+++ components/Article.tsx
@@ -0,0 +1,47 @@
+import styled, {css} from 'styled-components';
+import Flex from 'components/Flex';
+import Heading from 'components/Heading';
+
+interface ArticleProps {
+  articleId: string;
+}
+
+function Article({articleId}: ArticleProps) {
+  return (
+    <article>
+      <Flex
+        justifyContent="space-between"
+        css={css`
+          margin-bottom: ${({theme}) => theme.spaces.large};
+        `}
+      >
+        <Category>Category</Category>
+        <Date>Date</Date>
+      </Flex>
+      <Heading
+        css={css`
+          margin-bottom: ${({theme}) => theme.spaces.small};
+        `}
+      >
+        Heading
+      </Heading>
+      <Description>Description</Description>
+    </article>
+  );
+}
+
+const Category = styled.span`
+  color: ${({theme}) => theme.colors['green-40']};
+  font-size: ${({theme}) => theme.fontSizes.small};
+  letter-spacing: 3px;
+`;
+
+const Date = styled.time`
+  color: ${({theme}) => theme.colors['gray-40']};
+`;
+
+const Description = styled.p`
+  color: ${({theme}) => theme.colors['gray-40']};
+`;
+
+export default Article;
