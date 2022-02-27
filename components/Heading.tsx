0385a8a1bd0d3516dabc6e776f3d85b4f5eba259
mass2527 <nzp01@naver.com>
2022-02-27 14:50:53 +0900

    add className props to Heading component

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -2,25 +2,26 @@ import {useContext} from 'react';
 import {LevelContext} from 'contexts/level';
 
 interface HeadingProps {
+  className?: string;
   children: string;
 }
 
-function Heading({children}: HeadingProps) {
+function Heading({className, children}: HeadingProps) {
   const level = useContext(LevelContext);
 
   switch (level) {
     case 1:
-      return <h1>{children}</h1>;
+      return <h1 className={className}>{children}</h1>;
     case 2:
-      return <h2>{children}</h2>;
+      return <h2 className={className}>{children}</h2>;
     case 3:
-      return <h3>{children}</h3>;
+      return <h3 className={className}>{children}</h3>;
     case 4:
-      return <h4>{children}</h4>;
+      return <h4 className={className}>{children}</h4>;
     case 5:
-      return <h5>{children}</h5>;
+      return <h5 className={className}>{children}</h5>;
     case 6:
-      return <h6>{children}</h6>;
+      return <h6 className={className}>{children}</h6>;
     default:
       throw Error('Unknown level: ' + level);
   }
