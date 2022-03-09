04607238fd2ebe93ae0f3ac379ba4718c275a7f7
mass2527 <nzp01@naver.com>
2022-03-09 16:35:12 +0900

    feat: :lipstick: add ResponsiveIFrame component

---
+++ components/ResponsiveIFrame.tsx
@@ -0,0 +1,39 @@
+import {ComponentProps} from 'react';
+import styled from 'styled-components';
+
+interface ResponsiveIFrameProps extends ComponentProps<'iframe'> {}
+
+const ResponsiveIFrame = ({
+  allow = 'accelerometer; encrypted-media; picture-in-picture; gyroscope;',
+  allowFullScreen = true,
+  ...rest
+}: ResponsiveIFrameProps) => {
+  return (
+    <Wrapper>
+      <iframe allow={allow} allowFullScreen={allowFullScreen} {...rest} />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+
+  &:after {
+    padding-top: 56.25%;
+    display: block;
+    content: '';
+  }
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: ${({theme}) => theme.radiuses.medium};
+  }
+`;
+
+export default ResponsiveIFrame;
