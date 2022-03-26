b90d42509ce4c1894223746f79ae0345ec2aac1f
mass2527 <nzp01@naver.com>
2022-03-26 23:00:23 +0900

    Rect 컴포넌트 타입 수정

--- components/Rect.tsx
+++ components/Rect.tsx
@@ -5,13 +5,13 @@ import {useRect, UseRectOptions} from 'hooks/useRect';
 interface RectProps {
   options: UseRectOptions;
   children: (args: {
-    ref: RefObject<HTMLElement | null>;
+    ref: RefObject<HTMLElement>;
     rect: DOMRect | null;
   }) => ReactNode;
 }
 
 const Rect = ({options, children}: RectProps) => {
-  const ref = useRef<HTMLElement | null>(null);
+  const ref = useRef<HTMLElement>(null);
   const rect = useRect(ref, options);
 
   return children({ref, rect});
